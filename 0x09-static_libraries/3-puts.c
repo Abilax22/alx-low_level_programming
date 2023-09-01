75b08506259d9c63c0e40e9096b4050b52ea7a04
Abilax22 <abiodunlawal4395@gmail.com>
2023-09-01 09:39:06 -0700

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - function prints a string to stdout
+ * @str: string to print
+ * _putchar prints a new line
+ */
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
+}
