7d677549656abcbc4069647e454ab1c0e288c138
Abilax22 <abiodunlawal4395@gmail.com>
2024-01-10 11:27:10 -0800

    1-print_rev_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,15 +4,3 @@
  * _puts_recursion - this function like puts ();
  * @s: input
  * return: Always 0
- */
-
-void _puts_recursion(char *s)
-{
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-		_putchar('\n');
-}
---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - this prints a string in a reverse
+ * @s: the string
+ * return: Always 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+		_putchar();
+}
