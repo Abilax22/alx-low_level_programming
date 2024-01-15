20b92094c71f84449a5c88f7702b021beb97e28c
Abilax22 <abiodunlawal4395@gmail.com>
2024-01-15 14:13:12 -0800

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,22 +2,21 @@
 #include <stdlib.h>
 
 /**
- * ct_word - this helper function count the no of words in a string
+ * ch_fr_grid - this helper function count the no of words in a string
  * @grid: string input
  * @height: string input
  * Return: number of words
  */
 
-int ct_word(char **grid, size_t height)
+int ch_fr_grid(char **grid, size_t height)
 {
-	if (grid != NULL height != 0)
+	if (grid != NULL && height != 0)
 	{
 		for (; height > 0; height--)
 			free(grid[height]);
 		free(grid);
 	}
 }
-
 /**
  * **strtow - this code splits a string into words
  * @str: string to split
@@ -35,9 +34,11 @@ char **strtow(char *str)
 	}
 
 	for (a = height = 0; str[a] != '\0'; a++)
-		if (str[a] != ' ' && (str[a + 1] == '  ' || str[a + 1] == '\0'))
+		if (str[a] != ' ' && (str[a + 1] == ' ' || str[a + 1] == '\0'))
 			height++;
-	pout = malloc(sizeof(char *) * (height = 1));
+	{
+		pout = malloc(sizeof(char *) * (height + 1));
+	}
 	if (pout == NULL)
 	{
 		free(pout);
@@ -54,13 +55,13 @@ char **strtow(char *str)
 				pout[c] = malloc((a - end + 2) * sizeof(char));
 				if (pout[c] == NULL)
 				{
-					ct_word(pout, c);
+					ch_fr_grid(pout, c);
 					return (NULL);
 				}
 				break;
 			}
 		}
-		for (j = 0; end <= a; end++; j++)
+		for (j = 0; end <= a; end++, j++)
 			pout[c][j] = str[end];
 		pout[c][j] = '\0';
 	}
